e3ddde44e242d6c30b00a910d2d18f0e71fc76e9
Isabella Aguilar <84691217+isabellaaguilar@users.noreply.github.com>
2022-10-12 08:46:02 -0600

    Update script.js
    nueva fecha

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 27, 2022 7:00:00").getTime();
+var countDownDate = new Date("Nov 03, 2022 7:10:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
